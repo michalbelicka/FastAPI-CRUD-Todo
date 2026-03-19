a69480fd6f1745d0d0ec9f9a9aeeb317f5ba48aa
michalbelicka <michal.belicka2310@gmail.com>
2026-03-19 16:37:24 +0100

    Add PUT test with invalid title

---
+++ tests/test_put_invalid_title.py
@@ -0,0 +1,54 @@
+import pytest
+from fastapi.testclient import TestClient
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from main import app
+
+client = TestClient(app)
+
+@pytest.fixture
+def create_todo():
+
+    post_data = {
+        "title": "string",
+        "description": "string",
+        "status": "pending"
+    }
+
+    response = client.post("/todos", json=post_data)
+
+    assert response.status_code == 200
+    assert "id" in response.json()
+
+    todo_id = response.json()["id"]
+
+    yield todo_id
+
+    client.delete(f"/todos/{todo_id}")
+
+def test_put_invalid_title(create_todo):
+
+    todo_id = create_todo
+
+    put_data = {
+        "title": {},
+        "description": "string",
+        "status": "pending"
+    }
+
+    response = client.put(f"/todos/{todo_id}", json=put_data)
+    assert response.status_code == 422
+    
+    response_json = response.json()
+    
+    assert "detail" in response_json
+    assert "body" in response_json["detail"][0]["loc"]
+    assert "title" in response_json["detail"][0]["loc"]
+
+    assert response_json["detail"][0]["msg"] == "str type expected"
+    assert response_json["detail"][0]["type"] == "type_error.str"
+        
+    
+    
+
