65b659938ab1a9701ba0306bf819896f25106e4d
michalbelicka <michal.belicka2310@gmail.com>
2026-03-30 12:15:24 +0200

    Add conftest.py with override function and fixture and test_db setup

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import os
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from database.database import get_db
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from main import app
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEST_DB_PATH = os.path.join(BASE_DIR, "test_db")
+TEST_DB_URL = f"sqlite:///{TEST_DB_PATH}"
+
+engine_test = create_engine(TEST_DB_URL, connect_args={"check_same_thread": False})
+SessionLocalTest = sessionmaker(autocommit=False, autoflush=False, bind=engine_test)
+
+
+def override_get_db():
+    db = SessionLocalTest()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@pytest.fixture(scope="module")
+def client():
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as c:
+        yield c
